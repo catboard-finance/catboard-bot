a4a1b9f6f13384e08b6f9d292f8e7d03bff486c7
katopz <katopz@gmail.com>
2022-01-30 23:14:26 +0700

    remove embeds

--- src/discord/command/hello.rs
+++ src/discord/command/hello.rs
@@ -1,25 +1,23 @@
-use serde_json::json;
-
 use crate::discord::interaction::{
     InteractionApplicationCommandCallbackData, InteractionResponse, InteractionResponseType,
 };
 
 pub(crate) async fn hello() -> InteractionResponse {
-    let content = "hello there!".to_string();
-    let embed = json!({
-      "type": "rich",
-      "title": "Solana",
-      "description": "foo",
-      "color": 0x8400ff,
-    })
-    .to_string();
-    let embeds: Vec<String> = [embed].to_vec();
+    let content = "Hello there!".to_string();
+    // let embed = json!({
+    //   "type": "rich",
+    //   "title": "Solana",
+    //   "description": "foo",
+    //   "color": 0x8400ff,
+    // })
+    // .to_string();
+    // let embeds: Vec<String> = [embed].to_vec();
 
     InteractionResponse {
         ty: InteractionResponseType::ChannelMessageWithSource,
         data: Some(InteractionApplicationCommandCallbackData {
             content: content,
-            embeds: Some(embeds),
+            embeds: None,
         }),
     }
 }
